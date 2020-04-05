f9a76bf2f197fa00630ce82c54745652242d78d1
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2020-04-05 03:12:50 +0100

    Update witnessprotection.lua

--- witnessprotection/witnessprotection.lua
+++ witnessprotection/witnessprotection.lua
@@ -1,5 +1,5 @@
 _addon.name = 'Witness Protection'
-_addon.version = '0.0.7'
+_addon.version = '0.0.8'
 _addon.author = 'Lili'
 _addon.command = 'wit'
 
@@ -13,25 +13,25 @@ function dbg(...)
 end
 
 local fields = {
-	[0x00A] = {['name_key'] = 'Player Name',	has_index = true,	['index_key'] = 'Player'		},
-	[0x00D] = {['name_key'] = 'Character Name',	has_index = true,	['index_key'] = 'Player'		},
-	[0x0DD] = {['name_key'] = 'Name', 			has_index = true,	['index_key'] = 'Index'			},
+	[0x00A] = {['name_key'] = 'Player Name',	has_id = true,	['index_key'] = 'Player'		},
+	[0x00D] = {['name_key'] = 'Character Name',	has_id = true,	['index_key'] = 'Player'		},
+	[0x0DD] = {['name_key'] = 'Name', 			has_id = true,	['index_key'] = 'ID'			},
 
-	[0x027] = {['name_key'] = 'Player Name',	has_index = true,	['index_key'] = 'Player Index'	},
-	[0x017] = {['name_key'] = 'Sender Name',	has_index = false,									},	
-	[0x0B6] = {['name_key'] = 'Target Name',	has_index = false,									},
-	[0x078] = {['name_key'] = 'Proposer Name',	has_index = true,	['index_key'] = 'Proposer Index'},
-	[0x079] = {['name_key'] = 'Proposer Name',	has_index = false,									},
-	[0x0CA] = {['name_key'] = 'Player Name',	has_index = false,									},
-	[0x0CC] = {['name_key'] = 'Player Name',	has_index = false,									},
-	[0x0DC] = {['name_key'] = 'Inviter Name',	has_index = false,									},
-	--[0x0E2] = {['name_key'] = 'Name',			has_index = true,	['index_key'] = 'Index'			}, -- Seems unnecessary
-	[0x106] = {['name_key'] = 'Name',			has_index = false									},
-	[0x107] = {['name_key'] = 'Name',			has_index = false									},
-	[0x108] = {['name_key'] = 'Name',			has_index = true,	['index_key'] = 'Index'			},
-	[0x109] = {['name_key'] = 'Buyer Name',		has_index = true,	['index_key'] = 'Buyer Index'	},
-	[0x10A] = {['name_key'] = 'Buyer Name',		has_index = false									},	
-	--[0x070] = {['name_key'] = 'Player Name'	has_index = false, --Untested
+	[0x027] = {['name_key'] = 'Player Name',	has_id = true,	['index_key'] = 'Player'		},
+	[0x017] = {['name_key'] = 'Sender Name',	has_id = false,									},	
+	[0x0B6] = {['name_key'] = 'Target Name',	has_id = false,									},
+	[0x078] = {['name_key'] = 'Proposer Name',	has_id = true,	['index_key'] = 'Proposer ID'	},
+	[0x079] = {['name_key'] = 'Proposer Name',	has_id = false,									},
+	[0x0CA] = {['name_key'] = 'Player Name',	has_id = false,									},
+	[0x0CC] = {['name_key'] = 'Player Name',	has_id = false,									},
+	[0x0DC] = {['name_key'] = 'Inviter Name',	has_id = false,									},
+	--[0x0E2] = {['name_key'] = 'Name',			has_id = true,	['index_key'] = 'ID'			}, -- Seems unnecessary
+	[0x106] = {['name_key'] = 'Name',			has_id = false									},
+	[0x107] = {['name_key'] = 'Name',			has_id = false									},
+	[0x108] = {['name_key'] = 'Name',			has_id = true,	['index_key'] = 'ID'			},
+	[0x109] = {['name_key'] = 'Buyer Name',		has_id = true,	['index_key'] = 'Buyer ID'		},
+	[0x10A] = {['name_key'] = 'Buyer Name',		has_id = false									},	
+	--[0x070] = {['name_key'] = 'Player Name'	has_id = false, }--Untested
 }
 
 local name_cache = { -- Can put in here any default renaming you want
@@ -86,7 +86,7 @@ windower.register_event('incoming chunk',function(id, original, modified, inject
 		
 		if name_cache[original_name] then
 			new_name = name_cache[original_name]
-		elseif fields[id].has_index then
+		elseif fields[id].has_id then
 			local index = p[fields[id].index_key]
 			new_name = syllabize(index):gsub("^%l", string.upper)
 			name_cache[original_name] = new_name
