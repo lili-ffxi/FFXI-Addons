b0993fd41aa5e80f22be879e119a70538b0ecce8
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2022-06-20 00:58:27 +0100

    Add files via upload

---
+++ collector/collections.lua
@@ -0,0 +1,72 @@
+return T{
+    ['reive capes'] = { "Mauler's Mantle", "Anchoret's Mantle", "Mending Cape", "Bane Cape", "Ghostfyre Cape", "Canny Cape", 
+                        "Weard Mantle", "Niht Mantle", "Pastoralist's Mantle", "Rhapsode's Cape", "Lutian Cape", "Takaha Mantle", 
+                        "Yokaze Mantle", "Updraft Mantle", "Conveyance Cape", "Cornflower Cape", "Gunslinger's Cape", 
+                        "Dispersal Mantle", "Toetapper Mantle", "Bookworm's Cape", "Lifestream Cape", "Evasionist's Cape", },
+    ['empyrean feet'] = {"Ravager's Calligae","Tantra Gaiters","Orison Duckbills","Goetia Sabots","Estq. Houseaux",
+                        "Raider's Poulaines","Creed Sabatons","Bale Sollerets","Ferine Ocreae","Aoidos' Cothurnes",
+                        "Sylvan Bottillons","Unkai Sune-Ate","Iga Kyahan","Lncr. Schynbalds","Caller's Pigaches","Mavi Basmak",
+                        "Navarch's Bottes","Cirque Scarpe","Charis Toe Shoes","Savant's Loafers"}, -- geo and run cannot be exchanged with Lame Deer
+    ['ambuscade weapons'] = {"Karambit", "Tauret", "Naegling", "Nandaka", "Dolichenus", "Lycurgos", "Drepanum", "Shining One", "Gokotai", "Hachimonji", "Maxentius", "Xoanon", "Ullr", "Khonsu", },
+    ['pulse weapons'] = {"Sagasinger", "Murasamemaru", "Tenkomaru", "Himthige", "Aytanri", "Adflictio", "Girru", "Gusterion", "Dukkha", "Ephemeron", "Coruscanti", "Asteria", "Borealis", "Ikarigiri", "Delphinius", "Chastisers", "Router", "Annealed Lance", },
+    ['mission rewards'] = { "Bastokan Flag", "Windurstian Flag", "San d'Orian Flag", -- rank 10
+                            "Abyssal Earring", "Beastly Earring", "Bushinomimi", "Knight's Earring", "Suppanomimi", -- zilart
+                            "Dcl.Grd. Ring", "Tavnazian Ring", "Rajas Ring", "Sattva Ring", "Tamas Ring", -- cop
+                            "Ethereal Earring", "Hollow Earring", "Magnetic Earring", "Static Earring", -- zilart/cop epilogue
+                            "Balrahn's Ring", "Jalzahn's Ring","Ulthalam's Ring","Glory Crown", -- toau
+                            "Jeunoan Flag", "Fourth Staff", "Ram Staff", "Cobra Staff", "Moonshade Earring", --wotg
+                            "Mirke Wardecors", "Nuevo Coselete", "Royal Redingote", 
+                            "Champion's Galea", "Anwig Salade", "Selenian Cap", 
+                            "Blitzer Poleyn", "Desultor Tassets", "Tatsu. Sitagoromo", }, -- addon scenarions
+    ['adoulin'] = { "Adoulin's Refuge", "Ygnas's Resolve", "Arciela's Grace", "Adoulin's Refuge +1", "Ygnas's Resolve +1", "Arciela's Grace +1", -- adoulin has _way_ too much possible stuff.
+                    "Councilor's Cuffs", "Councilor's Garb", 
+                    "Adoulin Ring", "Gorney Ring", "Haverton Ring", "Janniston Ring", "Karieyh Ring", "Orvail Ring", 
+                    "Renaye Ring", "Shneddick Ring", "Thurandaut Ring", "Vocane Ring", "Weather. Ring", "Woltaris Ring", 
+                    "Delegate's Cuffs", "Delegate's Garb", 
+                    "Adoulin Ring +1", "Gorney Ring +1", "Haverton Ring +1", "Jann. Ring +1", "Karieyh Ring +1", "Orvail Ring +1", 
+                    "Renaye Ring +1", "Shneddick Ring +1", "Thur. Ring +1", "Vocane Ring +1", "Weather. Ring +1", "Woltaris Ring +1", },
+    ['twilight'] = { "Twilight Cape", "Twilight Knife", "Twilight Scythe", "Twilight Helm", "Twilight Mail", "Twilight Cloak", "Twilight Torque", "Twilight Belt", }, -- twilight set from Shinryu
+    ['dynamis currency'] = {"1 Byne Bill", "O. Bronzepiece", "T. Whiteshell", "100 Byne Bill", "M. Silverpiece", "L. Jadeshell", "10,000 Byne Bill", "R. Goldpiece", "R. Stripeshell",},
+    ['relic'] = { "Aegis", "Gjallarhorn", "Mandau", "Ragnarok", "Apocalypse", "Annihilator", "Amanomurakumo", "Excalibur", "Yoichinoyumi", "Spharai", "Kikoku", "Bravura", "Gungnir", "Guttler", "Mjollnir", "Claustrum", },
+    ['mythic'] = { "Conqueror", "Glanzfaust", "Yagrush", "Laevateinn", "Murgleis", "Vajra", "Burtgang", "Liberator", "Aymur", "Carnwenhan", "Gastraphetes", "Kogarasumaru", "Nagi", "Ryunohige", "Nirvana", "Tizona", "Death Penalty", "Kenkonken", "Terpsichore", "Tupsimati", "Idris", "Epeolatry", },
+    ['empyrean'] = { "Verethragna", "Twashtar", "Almace", "Caladbolg", "Farsha", "Ukonvasara", "Redemption", "Rhongomiant", "Kannagi", "Masamune", "Gambanteinn", "Hvergelmir", "Gandiva", "Armageddon", "Daurdabla", "Ochain", },
+    ['aeonic'] = { "Godhands", "Aeneas", "Sequence", "Lionheart", "Tri-edge", "Chango", "Anguta", "Trishula", "Heishi Shorinken", "Dojikiri Yasutsuna", "Tishtrya", "Khatvanga", "Fail-Not", "Fomalhaut", "Marsyas", "Srivatsa", },
+    ['combatant torque'] = { "Combatant's Torque", "Carnal Torque", "Decimus Torque", "Bilious Torque", "Agelast Torque", "Maskirova Torque", "Yarak Torque", "Acantha Torque", },
+    ['incanter torque'] = { "Incanter's Torque", "Melic Torque", "Henic Torque", "Deceiver's Torque", },
+    ['eschan torques'] = { "Combatant's Torque", "Incanter's Torque", "Carnal Torque", "Decimus Torque", "Bilious Torque", "Agelast Torque", "Maskirova Torque", "Yarak Torque", "Acantha Torque", "Melic Torque", "Henic Torque", "Deceiver's Torque", },
+    ['elemental gorgets'] = { "Flame Gorget", "Soil Gorget", "Aqua Gorget", "Breeze Gorget", "Snow Gorget", "Thunder Gorget", "Light Gorget", "Shadow Gorget", "Fotia Gorget",  },
+    ['elemental belts'] = { "Flame Belt", "Soil Belt", "Aqua Belt", "Breeze Belt", "Snow Belt", "Thunder Belt", "Light Belt", "Shadow Belt", "Fotia Belt",  },
+    ['elemental obi'] = { "Karin Obi", "Dorin Obi", "Suirin Obi", "Furin Obi", "Hyorin Obi", "Rairin Obi", "Korin Obi", "Anrin Obi", "Hachirin-no-Obi", },
+    ['sinister reign'] = {  "Himetsuruichimonji", "Humility", "Lengo Pants", "Leyline Gloves",  -- Arciela
+                            "Amm Greaves", "Fleshcarvers", "Koresuke", "Ta'lab Trousers",       -- Darrcuiln
+                            "Dampening Tam", "Fanatic Gloves", "Malevolence", "Purgation",      -- Ingrid
+                            "Rubicundity", "Samnuha Coat", "Samnuha Tights", "Vampirism",       -- Teodor
+                            "Brutality", "Ferocity", "Lilitu Headpiece", "Loyalist Sabatons",   -- Morimar
+                            "Floral Gauntlets", "Medium's Sabots", "Nobility", "Serenity",      -- Rosulatia
+                            "Enticer's Pants", "Ochu", "Taming Sari", "Witching Robe",          -- Arciela
+                            "Brilliance", "Divinity", "Jumalik Helm", "Jumalik Mail",           -- Sajj'aka
+                            "Founder's Corona", "Found. Breastplate", "Founder's Gauntlets", "Founder's Hose", "Founder's Greaves", -- August
+                            },
+    ['reisenjima helm'] = { "Firangi", "Freydis", "Gozuki Mezuki", "Hodadenon", "Izcalli", "Lembing", "Misanthropy", "Oranyan", "Sangoma", "Shishio", "Steinthor", "Suwaiyas", "Taka", "Takoba", "Wochowsen",
+                            "Ahosi Leggings", "Arjuna Breeches", "Composer's Mitts", "Composer's Sabots", "Iktomi Dastanas", "Ipoca Beret", "Mrigavyadha Gloves", "Navon Crackows", "Nzingha Cuirass", "Sayadio's Kaftan", "Skaoi Boots", "Vedic Coat", "Ynglinga Sallet", },
+    ['skirmish stones'] = {
+                            "Snowslit Stone", "Snowslit Stone +1", "Snowslit Stone +2",
+                            "Snowtip Stone", "Snowtip Stone +1", "Snowtip Stone +2",
+                            "Snowdim Stone", "Snowdim Stone +1", "Snowdim Stone +2",
+                            "Snoworb Stone", "Snoworb Stone +1", "Snoworb Stone +2",
+                            "Leafslit Stone", "Leafslit Stone +1", "Leafslit Stone +2",
+                            "Leaftip Stone", "Leaftip Stone +1", "Leaftip Stone +2",
+                            "Leafdim Stone", "Leafdim Stone +1", "Leafdim Stone +2",
+                            "Leaforb Stone", "Leaforb Stone +1", "Leaforb Stone +2",
+                            "Duskslit Stone", "Duskslit Stone +1", "Duskslit Stone +2",
+                            "Dusktip Stone", "Dusktip Stone +1", "Dusktip Stone +2",
+                            "Duskdim Stone", "Duskdim Stone +1", "Duskdim Stone +2",
+                            "Duskorb Stone", "Duskorb Stone +1", "Duskorb Stone +2",    },
+    -- HTBFs
+    ['lilith htbf'] = { "Daybreak","Malignance Pole","Malignance Sword","Malignance Chapeau","Malignance Tabard","Malignance Gloves","Malignance Tights","Malignance Boots","Malignance Earring"},
+    ['odin htbf'] = {"Geirrothr","Zantetsuken","Zantetsuken X","Hjarrandi Helm","Hjarrandi Breast.","Freke Ring","Gere Ring"},
+    ['alex htbf'] = {"Sacro Bulwark","Sacro Breastplate","Sacro Gorget","Sacro Cord","Sacro Mantle",},
+    ['shinryu htbf'] = {"Crepuscular Knife","Crepuscular Pebble","Crepuscular Scythe","Crepuscular Helm","Crepuscular Cloak","Crepuscular Mail","Crepuscular Ring","Crepuscular Earring"},
+}
+
+-- TODO: make it check key items too
---
+++ collector/collector.lua
@@ -0,0 +1,159 @@
+_addon.name = 'Collector'
+_addon.author = 'Lili'
+_addon.version = '0.0.3'
+_addon.subversion = 1
+_addon.commands = {'memnon','collection','col'}
+
+require('chat')
+require('logger')
+require('tables')
+
+local slips = require('slips')
+local res_items = require('resources').items
+
+local collections = require('collections') 
+
+bags = L{'safe','safe2','storage','locker','inventory','satchel','sack','case','wardrobe','wardrobe2','wardrobe3','wardrobe4','wardrobe5','wardrobe6','wardrobe7','wardrobe8',}
+
+sorted_bags = L{'safe', 'safe2', 'storage', 'locker', 
+                'satchel', 'sack', 'case', 'inventory', 
+                'wardrobe', 'wardrobe2', 'wardrobe3','wardrobe4', 
+                'wardrobe5', 'wardrobe6', 'wardrobe7','wardrobe8', 
+                'slip 01', 'slip 02', 'slip 03', 'slip 04', 'slip 05', 'slip 06', 'slip 07', 'slip 08', 'slip 09', 'slip 10', 
+                'slip 11', 'slip 12', 'slip 13', 'slip 14', 'slip 15', 'slip 16', 'slip 17', 'slip 18', 'slip 19', 'slip 20', 
+                'slip 21', 'slip 22', 'slip 23', 'slip 24', 'slip 25', 'slip 26', 'slip 27', 'slip 28', }
+
+function add_result(result,bag,count)
+    local count = count > 1 and ' ('..count..')' or ''
+    return (bag == 'missing' and result:color(259) or result:color(258)) .. count
+end
+
+function curate(set)
+    
+    local collection = L(collections[set])
+    local inventory = windower.ffxi.get_items()
+
+    local results = T{
+                missing = collection:copy(),
+                owned = L{},
+            }
+    
+	for _,bag in ipairs(bags) do 
+		for i = 1, inventory[bag].max do
+			data = inventory[bag][i]
+			if data.id ~= 0 then
+                local name = res_items[data.id].name
+                if collection:contains(name) then
+                    if not results[bag] then
+                        results[bag] = L{}
+                    end
+                    results[bag]:append(add_result(name,bag,data.count))
+                    local m = results.missing:find(name)
+                    if m then
+                        results.missing:remove(m)
+                    end
+                    results.owned:append(name)
+                end
+			end
+        end
+    end
+
+    local slip_storages = slips.get_player_items()
+    for _, slip_id in ipairs(slips.storages) do
+        local slip_name = 'slip '..tostring(slips.get_slip_number_by_id(slip_id)):lpad('0', 2)
+        for _, id in ipairs(slip_storages[slip_id]) do
+            local name = res_items[id].name
+            if collection:contains(name) then
+                if not results[slip_name] then
+                    results[slip_name] = L{}
+                end
+                results[slip_name]:append(add_result(name,slip_name,1))
+                local m = results.missing:find(name)
+                if m then
+                    results.missing:remove(m)
+                end
+                results.owned:append(name)                
+            end
+        end
+    end
+    
+    --table.vprint(results)
+    
+    log('Results:')
+    for i=#results.missing,1,-1 do
+        local name = results.missing[i]
+        local item = res_items:with('name',name)
+        if not item then
+            log('invalid item:',name:color(123)..'.','Check the spelling!')
+            results.missing:remove(i)
+        else
+            log('missing:',name:color(259))
+        end
+    end
+
+    for _,bag in ipairs(sorted_bags) do 
+        if results[bag] then
+            for _,item in ipairs(results[bag]) do
+                log('%s: %s':format(bag,item))
+            end
+        end
+    end
+    
+    log(set..':\n',results.owned.n,'owned /',results.missing.n,'missing')
+    if results.owned.n + results.missing.n > #collection then
+        log(' ' .. #collection,'unique items in the set.')
+    end
+end
+
+function cmd(args)
+    windower.send_command(args)
+end
+
+windower.register_event('addon command', function(...)
+    
+    local args = T{...}
+    local mode = args[1] and args[1]:lower() or 'help'
+    
+    if mode == 'r' or mode == 'reload' then
+        cmd('lua r '.._addon.name)
+        return
+        
+    elseif mode == 'u' or mode == 'unload' then
+        cmd('lua u '.._addon.name)
+        return
+        
+    elseif mode == 'missing' then
+        table.remove(args,1)
+        log('missing only')
+        
+    elseif mode == 'owned' then
+        table.remove(args,1)
+        log('owned only')
+        
+    else
+        mode = default_mode
+        
+    end
+    
+    local arg = args:concat(' '):lower()
+    
+    if collections[arg] then
+        curate(arg)
+    else
+        if arg ~= 'help' then
+            log('Collection not found: '..arg)
+        end
+        
+        log('Usage:\n//col <collection name>\nAvailable collections:')
+        for k,v in pairs(collections) do
+            if #v > 1 then 
+                log('\t',k)
+            end
+        end
+    end
+end)
+
+--[[
+Curation is about knowing which pieces belong together.
+                          -- Memnon Delphius Vanderbeam
+]]
