fc5beebf50d6de012294548edca35a7ed3240111
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2024-02-10 22:30:52 +0000

    Update collections.lua
    Updated item sets.

--- collector/collections.lua
+++ collector/collections.lua
@@ -1,4 +1,4 @@
-_addon.subversion = '9'
+_addon.subversion = '10'
 
 local custom = T{}
 if windower.file_exists(windower.addon_path..'custom.lua') then
@@ -15,19 +15,20 @@ return T{
                         "Raider's Poulaines","Creed Sabatons","Bale Sollerets","Ferine Ocreae","Aoidos' Cothurnes",
                         "Sylvan Bottillons","Unkai Sune-Ate","Iga Kyahan","Lncr. Schynbalds","Caller's Pigaches","Mavi Basmak",
                         "Navarch's Bottes","Cirque Scarpe","Charis Toe Shoes","Savant's Loafers"}, -- geo and run cannot be exchanged with Lame Deer
-    ["vagary key items"] = {"fabricated ward of impurity","fabricated ward of miasma","fabricated ward of biting winds","fabricated ward of ashen wings","fabricated ward of the false king",},
-    ["vagary leader"] = {"prototype sigil pearl","prototype pearl of ashen wings","prototype pearl of the false king",},
-    ["omen boss beads"] = {"Fu's bead","Kei's bead","Kyou's bead","Gin's bead","Kin's bead",},
-    ["geas fete zitah pops"] = {"Wepwawet's tooth","Lydia's vine","Aglaophotis bud","Tangata's wing","Vidala's claw","Gestalt's retina","Angrboda's necklace","Cunnast's talon","Revetaur's horn","Ferrodon's scale","Gulltop's shell","Vyala's prey","Blazewing's pincer","Coven's dust","Pazuzu's blade hilt","Wrathare's carrot","Ionos's webbing","Sandy's lasher","Nosoi's feather","Brittlis's ring","Kamohoalii's fin","Umdhlebi's flower","Fleetstalker's claw","Shockmaw's blubber","Urmahlullu's armor",},
-    ["geas fete ruaun pops"] = {"Bia's glove","Ruea's stone","Ma's lance","Khon's scepter","Met's ring","Khun's crown","Wasserspeier's horn","Emputa's wing","Peirithoos's hoof","Asida's gel","Tenodera's scythe","Sava Savanovic's cape","Palila's talon","Hanbi's nail","Yilan's scale","Amymone's tooth","Naphula's bracelet","Kammavaca's binding","Pakecet's blubber","Duke Vepar's signet","Vir'ava's stalk","Byakko's Pride","Genbu's Honor","Seiryu's Nobility","Suzaku's Benefaction","Kirin's Fervor","Ark Angel HM's coat","Ark Angel TT's necklace","Ark Angel MR's buckle","Ark Angel EV's sash","Ark Angel GK's bangle","primal nazar",},
-    ["geas fete reisenjima pops"] = {"Crom Dubh's helm","Golden Kist's key","Mauve-wristed Gomberry's knife","Dazzling Dolores's vine","Taelmoth's staff","Belphegor's crown","Kabandha's wing","Selkit's pincer","Sang Buaya's tusk","Sabotender Royal's needle","Zduhac's talon","Oryx's plumage","Strophadia's pearl","Gajasimha's mane","Ironside's maul","Sarsaok's hoard","Old Shuck's tuft","Bashmu's trinket","Maju's claw","Yakshi's scroll","Neak's treasure","Teles's hymn","Zerde's cup","Vinipata's blade","Schah's gambit","Albumen's flower","Onychophora's soil","Erinys's beak",},
-     ["nazars"] = {"primary nazar","secondary nazar","tertiary nazar","quaternary nazar","quinary nazar","senary nazar","septenary nazar","octonary nazar","nonary nazar","denary nazar",},
+    ['vagary key items'] = {"fabricated ward of impurity","fabricated ward of miasma","fabricated ward of biting winds","fabricated ward of ashen wings","fabricated ward of the false king",},
+    ['vagary leader'] = {"prototype sigil pearl","prototype pearl of ashen wings","prototype pearl of the false king",},
+    ['omen boss beads'] = {"Fu's bead","Kei's bead","Kyou's bead","Gin's bead","Kin's bead",},
+    ['geas fete zitah pops'] = {"Wepwawet's tooth","Lydia's vine","Aglaophotis bud","Tangata's wing","Vidala's claw","Gestalt's retina","Angrboda's necklace","Cunnast's talon","Revetaur's horn","Ferrodon's scale","Gulltop's shell","Vyala's prey","Blazewing's pincer","Coven's dust","Pazuzu's blade hilt","Wrathare's carrot","Ionos's webbing","Sandy's lasher","Nosoi's feather","Brittlis's ring","Kamohoalii's fin","Umdhlebi's flower","Fleetstalker's claw","Shockmaw's blubber","Urmahlullu's armor",},
+    ['geas fete ruaun pops'] = {"Bia's glove","Ruea's stone","Ma's lance","Khon's scepter","Met's ring","Khun's crown","Wasserspeier's horn","Emputa's wing","Peirithoos's hoof","Asida's gel","Tenodera's scythe","Sava Savanovic's cape","Palila's talon","Hanbi's nail","Yilan's scale","Amymone's tooth","Naphula's bracelet","Kammavaca's binding","Pakecet's blubber","Duke Vepar's signet","Vir'ava's stalk","Byakko's Pride","Genbu's Honor","Seiryu's Nobility","Suzaku's Benefaction","Kirin's Fervor","Ark Angel HM's coat","Ark Angel TT's necklace","Ark Angel MR's buckle","Ark Angel EV's sash","Ark Angel GK's bangle","primal nazar",},
+    ['geas fete reisenjima pops'] = {"Crom Dubh's helm","Golden Kist's key","Mauve-wristed Gomberry's knife","Dazzling Dolores's vine","Taelmoth's staff","Belphegor's crown","Kabandha's wing","Selkit's pincer","Sang Buaya's tusk","Sabotender Royal's needle","Zduhac's talon","Oryx's plumage","Strophadia's pearl","Gajasimha's mane","Ironside's maul","Sarsaok's hoard","Old Shuck's tuft","Bashmu's trinket","Maju's claw","Yakshi's scroll","Neak's treasure","Teles's hymn","Zerde's cup","Vinipata's blade","Schah's gambit","Albumen's flower","Onychophora's soil","Erinys's beak",},
+     ['nazars'] = {"primary nazar","secondary nazar","tertiary nazar","quaternary nazar","quinary nazar","senary nazar","septenary nazar","octonary nazar","nonary nazar","denary nazar",},
 
-    ["ambuscade cape materials"] = {"A. Voucher: Back","Abdhaljs Thread","Abdhaljs Dust","Abdhaljs Dye","Abdhaljs Sap","Abdhaljs Resin","Abdhaljs Needle",},
-    ["ambuscade weapons materials"] = {"A. Voucher: Weapon","Abdhaljs Nugget","Abdhaljs Gem","Abdhaljs Anima","Abdhaljs Matter",},
+    ['ambuscade cape materials'] = {"A. Voucher: Back","Abdhaljs Thread","Abdhaljs Dust","Abdhaljs Dye","Abdhaljs Sap","Abdhaljs Resin","Abdhaljs Needle",},
+    ['ambuscade weapons materials'] = {"A. Voucher: Weapon","Abdhaljs Nugget","Abdhaljs Gem","Abdhaljs Anima","Abdhaljs Matter",},
     ['ambuscade weapons'] = {"Karambit", "Tauret", "Naegling", "Nandaka", "Dolichenus", "Lycurgos", "Drepanum", "Shining One", "Gokotai", "Hachimonji", "Maxentius", "Xoanon", "Ullr", "Khonsu", },
     ['ambuscade replicas'] = {"Tzee Xicu's Blade", "Sophistry", "Soulflayer's Wand", "Burrower's Wand", "Levin", "Lamia Staff", "Dullahan Shield", "Goujian", "Dullahan Axe", "Gramk's Axe", "Profane Staff", "Savage. Pole", "Troll Gun", "Ranine Staff", "Autarch's Axe", "Qutrub Knife", "Za'Dha Chopper", "Helgoland", "Ophidian Sword",},
     ['pulse weapons'] = {"Sagasinger", "Murasamemaru", "Tenkomaru", "Himthige", "Aytanri", "Adflictio", "Girru", "Gusterion", "Dukkha", "Ephemeron", "Coruscanti", "Asteria", "Borealis", "Ikarigiri", "Delphinius", "Chastisers", "Router", "Annealed Lance", },
+    
     ['mission rewards'] = { "Bastokan Flag", "Windurstian Flag", "San d'Orian Flag", -- rank 10
                             "Abyssal Earring", "Beastly Earring", "Bushinomimi", "Knight's Earring", "Suppanomimi", -- zilart
                             "Dcl.Grd. Ring", "Tavnazian Ring", "Rajas Ring", "Sattva Ring", "Tamas Ring", -- cop
@@ -44,6 +45,7 @@ return T{
                     "Delegate's Cuffs", "Delegate's Garb", 
                     "Adoulin Ring +1", "Gorney Ring +1", "Haverton Ring +1", "Jann. Ring +1", "Karieyh Ring +1", "Orvail Ring +1", 
                     "Renaye Ring +1", "Shneddick Ring +1", "Thur. Ring +1", "Vocane Ring +1", "Weather. Ring +1", "Woltaris Ring +1",   },
+    
     ['monster mementos'] = { "abyssal wyrm memento", "abyssobugard memento", "adamantoise memento", "adenium memento", "ake-ome memento", 
                              "alabaster lizard memento", "baby adamantoise memento", "baby cockatrice memento", "baby colibri memento", 
                              "baby eft memento", "baby lizard memento", "baby rabbit memento", "baby raptor memento", "behemoth cub memento",
@@ -61,8 +63,9 @@ return T{
                              "sea monk memento", "sheep memento", "skormoth memento", "slime memento", "snoll memento", "tarichuk memento", 
                              "tiny bugard memento", "toucalibri memento", "uragnite memento", "uragnite youngling memento", 
                              "white adamantoise memento", "white rabbit memento", "wyvern memento", "ziz memento"    },
-    ['twilight'] = { "Twilight Cape", "Twilight Knife", "Twilight Scythe", "Twilight Helm", "Twilight Mail", "Twilight Cloak", "Twilight Torque", "Twilight Belt", }, -- twilight set from Shinryu
+    ['twilight armor'] = { "Twilight Cape", "Twilight Knife", "Twilight Scythe", "Twilight Helm", "Twilight Mail", "Twilight Cloak", "Twilight Torque", "Twilight Belt", }, -- twilight set from Shinryu
     ['dynamis currency'] = {"1 Byne Bill", "O. Bronzepiece", "T. Whiteshell", "100 Byne Bill", "M. Silverpiece", "L. Jadeshell", "10,000 Byne Bill", "R. Goldpiece", "R. Stripeshell",},
+    
     ['relic weapons'] = { "Aegis", "Gjallarhorn", "Mandau", "Ragnarok", "Apocalypse", "Annihilator", "Amanomurakumo", "Excalibur", "Yoichinoyumi", "Spharai", "Kikoku", "Bravura", "Gungnir", "Guttler", "Mjollnir", "Claustrum", },
     ['mythic weapons'] = { "Conqueror", "Glanzfaust", "Yagrush", "Laevateinn", "Murgleis", "Vajra", "Burtgang", "Liberator", "Aymur", "Carnwenhan", "Gastraphetes", "Kogarasumaru", "Nagi", "Ryunohige", "Nirvana", "Tizona", "Death Penalty", "Kenkonken", "Terpsichore", "Tupsimati", "Idris", "Epeolatry", },
     ['vigil weapons'] = {"Sturdy Axe", "Burning Fists", "Werebuster", "Mage's Staff", "Vorpal Sword", "Swordbreaker", "Brave Blade", "Death Sickle", "Double Axe", "Dancing Dagger", "Killer Bow", "Windslicer", "Sasuke Katana", "Radiant Lance", "Scepter Staff", "Wightslayer", "Quicksilver", "Inferno Claws", "Main Gauche", "Elder Staff"},
@@ -71,12 +74,15 @@ return T{
     ['oboro weapons'] = {"Minos", "Nyepel", "Sindri", "Kaladanda", "Egeking", "Sandung", "Priwen", "Cronus", "Arktoi", "Terpander", "Lionsquall", "Kurikaranotachi", "Shigi", "Areadbhar", "Gridarvor", "Mimesis", "Deathlocke", "Ohtas", "Polyhymnia", "Coeus", "Dunna", "Aettir", },
     ['divergence weapons'] = {"Aram", "Asclepius", "Barfawc", "Bhima", "Crocea Mors", "Draumstafir", "Father Time", "Fudo Masamune", "Fusenaikyo", "Gandring", "Kaumodaki", "Labraunda", "Moralltach", "Morgelai", "Musa", "Pangu", "Rostam", "Sagitta", "Setan Kober", "Sharanga", "Xiucoatl", "Zomorrodnegar"},
     ['divergence necks'] = {"Abyssal Beads +2", "Argute Stole +2", "Asn. Gorget +2", "Bagua Charm +2", "Bard's Charm +2", "Bst. Collar +2", "Clr. Torque +2", "Comm. Charm +2", "Dgn. Collar +2", "Dls. Torque +2", "Etoile Gorget +2", "Futhark Torque +2", "Kgt. Beads +2", "Mirage Stole +2", "Mnk. Nodowa +2", "Ninja Nodowa +2", "Pup. Collar +2", "Sam. Nodowa +2", "Scout's Gorget +2", "Smn. Collar +2", "Src. Stole +2", "War. Beads +2",},
+    
     ['combatant torque'] = { "Combatant's Torque", "Carnal Torque", "Decimus Torque", "Bilious Torque", "Agelast Torque", "Maskirova Torque", "Yarak Torque", "Acantha Torque", },
     ['incanter torque'] = { "Incanter's Torque", "Melic Torque", "Henic Torque", "Deceiver's Torque", },
     ['eschan torques'] = { "Combatant's Torque", "Incanter's Torque", "Carnal Torque", "Decimus Torque", "Bilious Torque", "Agelast Torque", "Maskirova Torque", "Yarak Torque", "Acantha Torque", "Melic Torque", "Henic Torque", "Deceiver's Torque", },
+    
     ['elemental gorgets'] = { "Flame Gorget", "Soil Gorget", "Aqua Gorget", "Breeze Gorget", "Snow Gorget", "Thunder Gorget", "Light Gorget", "Shadow Gorget", "Fotia Gorget",  },
     ['elemental belts'] = { "Flame Belt", "Soil Belt", "Aqua Belt", "Breeze Belt", "Snow Belt", "Thunder Belt", "Light Belt", "Shadow Belt", "Fotia Belt",  },
     ['elemental obi'] = { "Karin Obi", "Dorin Obi", "Suirin Obi", "Furin Obi", "Hyorin Obi", "Rairin Obi", "Korin Obi", "Anrin Obi", "Hachirin-no-Obi", },
+    
     ['sinister reign'] = {  "Himetsuruichimonji", "Humility", "Lengo Pants", "Leyline Gloves",  -- Arciela
                             "Amm Greaves", "Fleshcarvers", "Koresuke", "Ta'lab Trousers",       -- Darrcuiln
                             "Dampening Tam", "Fanatic Gloves", "Malevolence", "Purgation",      -- Ingrid
@@ -87,6 +93,7 @@ return T{
                             "Brilliance", "Divinity", "Jumalik Helm", "Jumalik Mail",           -- Sajj'aka
                             "Founder's Corona", "Found. Breastplate", "Founder's Gauntlets", "Founder's Hose", "Founder's Greaves", -- August
                             },
+    
     ['reisenjima helm'] = { "Firangi", "Freydis", "Gozuki Mezuki", "Hodadenon", "Izcalli", "Lembing", "Misanthropy", "Oranyan", "Sangoma", "Shishio", "Steinthor", "Suwaiyas", "Taka", "Takoba", "Wochowsen",
                             "Ahosi Leggings", "Arjuna Breeches", "Composer's Mitts", "Composer's Sabots", "Iktomi Dastanas", "Ipoca Beret", "Mrigavyadha Gloves", "Navon Crackows", "Nzingha Cuirass", "Sayadio's Kaftan", "Skaoi Boots", "Vedic Coat", "Ynglinga Sallet", },
     ['skirmish stones'] = {
@@ -102,8 +109,8 @@ return T{
                             "Dusktip Stone", "Dusktip Stone +1", "Dusktip Stone +2",
                             "Duskdim Stone", "Duskdim Stone +1", "Duskdim Stone +2",
                             "Duskorb Stone", "Duskorb Stone +1", "Duskorb Stone +2",    },
-    ["omen rewards"] = {    "Adapa Shield", "Nusku Shield", "Anu Torque", "Sherida Earring", "Enki Strap", "Erra Pendant", "Kishar Ring", "Adad Amulet", 
-                            "Knobkierrie", "Ammurapi Shield", 
+    
+    ['omen rewards'] = {    "Adapa Shield", "Nusku Shield", "Anu Torque", "Sherida Earring", "Enki Strap", "Erra Pendant", "Kishar Ring", "Adad Amulet", "Knobkierrie", "Ammurapi Shield", 
                             "Nisroch Jerkin", "Ashera Harness", "Shamash Robe", "Dagon Breast.", "Udug Jacket", 
                             "Niqmaddu Ring", "Shulmanu Collar", "Dingir Ring", "Yamarang", "Lugalbanda Earring", "Ilabrat Ring", "Utu Grip","Enmerkar Earring", "Iskur Gorget", 
                             "Regal Cpt. Gloves", "Regal Cuffs", "Regal Gauntlets", "Regal Gloves", "Regal Belt", "Regal Earring", 
@@ -124,4 +131,33 @@ return T{
     ['znm drops'] = {"Vulpangue's Wing", "Chamrosh's Beak", "Gigiroon's Cape", "B. Borer's Cocoon", "Claret Globule", "Ob's Arm", "Velionis's Bone", "Lil' Apkallu's Egg", "Chigre", "Iriz Ima's Hide", "Iriri Samariri's Hat", "Amoosh.'s Tendril", "Anantaboga's Heart", "Reacton's Ashes", "Dextrose's Blubber", "Zrkl.'s Neckpiece", "Verdelet's Wing", "Wulgaru's Head", "A. Gears' Fragment", "G. Zha's Necklace", "Dea's Horn", "Nosferatu's Claw", "Bblr.'s Vambrace", "Acham.'s Antenna", "Mahjlaef's Staff", "Ex. Lamia Armband", "Nuhn's Esca", "Tinnin's Fang", "Sarameya's Hide", "Tyger's Tail", },
     ['exp items'] = {'Echad Ring','Caliber Ring','Emperor Band', 'Empress Band', 'Chariot Band', 'Resolution Ring', 'Allied Ring', 'Kupofried\'s Ring','Sprout Beret','Trizek Ring','Endorsement Ring','Facility Ring','Capacity Ring','Vocation Ring','Guide Beret',},
     ['woe weapons'] = {"Dumuzis","Khandroma","Brunello","Xiphias","Sacripante","Shamash","Umiliati","Daboya","Kasasagi","Torigashira","Rose Couverte","Paikea","Circinae","Mollfrith",},
+    
+    -- HELM pop sets for specific NMs
+    ['albumen'] = {'Vermihumus', 'Coalition Humus', 'Ashweed','Void Grass'},
+    ['erinys'] = {'tornado','mistmelt','Voidsnapper','Ashweed'},
+    ['onychophora'] = {'titanite','Worm Mulch','void crystal','void grass'},
+    ['schah'] = {'leisure table','trump card case','voidsnapper','gravewood log'},
+    ['teles'] = {"maiden's virelai","siren's hair",'void crystal','voidsnapper'},
+    ['vinipata'] = {'scarletite ingot', 'bone chip','Void Crystal','Duskcrawler'},
+    ['zerde'] = {'Black Pudding', 'Flan Meat', 'Void Grass','Ashen Crayfish'},
+    
+    -- Sheol NM pops
+    ['sheol a pop items'] = {"Abyssdiver Feather","Voso's Hide","Arthro's Shell","Veloz's Needle","Joyous's Moss", "Orcfeltrap's Leaf","Heiligtum's Moss","G. Gel's Mucus","Malatrix's Shard","Immani. Hide", "Tiyanak's Fang", "Warblade's Hide", "Samantha's Vine","Mender's Log","Intuila's Hide","Imperator's Wing"},
+    ['sheol b pop items'] = {"Vermillion's Wing","Specter's Ore","Centurio's Armor","Coca's Wing","Largantua's Shard","Azrael's Eye","Mephitas's Claw","Glazemane's Fang","Vidmapire's Claw","Clawberry's Coat","V. Cluster's Ash","Arke's Wing","Muut's Vestment","G. Grenade's Ash","Beist's Blood","Vedrfolnir's Wing","Jill's Spittle","Celine's Vine","Ayapec's Shell","Camahueto's Fur","Douma's Shard","Mhuufya's Beak","Ethereal Incense","Uropygid's Needle","Bakunawa's Ink","Bambrox's Shawl","Strix's Tailfeather"},
+    ['sheol c pop items'] = {"Hidhaegg's Scale", "Sovereign's Hide", "Tumult's Blood","Shedu's Mane","Tolba's Shell","Sarama's Hide","Thu'ban's Scale"},
+    
+    
+    -- assault books: todo augments on these
+    ['assault logs'] = {'Leujaoam Log','Mamool Ja Journal','Lebros Chronicle','Periqia Diary','Ilrusi Ledger'},
+    ['incursion key items']={"Ymmr-Ulvid's necklace", "Ignor-Mnt's bracelet","Durs-Vike's earring","Tryl-Wuj's belt","pair of Liij-Vok's gloves","Gramk-Droog's circlet" },
+
+    -- odyssey armor sets
+    ['sakpata']={"Sakpata's Sword","Sakpata's Plate","Sakpata's Leggings","Sakpata's Cuisses","Sakpata's Helm","Sakpata's Gauntlets"},
+    ['nyame']={"Nyame Helm","Nyame Mail","Nyame Gauntlets","Nyame Flanchard","Nyame Sollerets"},
+    ['gleti']={"Gleti's Mask","Gleti's Gauntlets","Gleti's Cuirass","Gleti's Breeches","Gleti's Boots","Gleti's Knife"},
+    ['bunzi']={"Bunzi's Hat", "Bunzi's Robe","Bunzi's Gloves","Bunzi's Pants", "Bunzi's Sabots", "Bunzi's Rod"},
+    ['mpaca']={"Mpaca's Staff","Mpaca's Cap","Mpaca's Doublet","Mpaca's Gloves","Mpaca's Hose","Mpaca's Boots"},
+    ['ikenga']={"Ikenga's Axe","Ikenga's Hat","Ikenga's Vest","Ikenga's Gloves","Ikenga's Trousers","Ikenga's Clogs"},
+    ['agwu']={"Agwu's Axe","Agwu's Cap","Agwu's Robe","Agwu's Gages","Agwu's Slops","Agwu's Pigaches"},
+
 }:update(custom)
