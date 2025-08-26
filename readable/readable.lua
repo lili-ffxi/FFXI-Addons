ab0fb2d2d1753f9fa08ba5c0fd2a3ec21beadee5
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2025-08-26 22:50:22 +0100

    Update readable.lua

--- readable/readable.lua
+++ readable/readable.lua
@@ -35,14 +35,12 @@ windower.register_event('load', function()
         local version_url = "https://raw.githubusercontent.com/lili-ffxi/FFXI-Addons/refs/heads/master/readable/readable.lua"
         local version_pattern = "_addon.version *= *['\"](.-)['\"]"
         local file_path = windower.addon_path .. "readable.lua"
-        
+
         get_txt(version_url, function(response, code, body)
             if code == 200 then
                 version = body:match(version_pattern)
 
-                print(_addon.version, version)                
-
-                if version and version ~= _addon.version then
+                    if version and version ~= _addon.version then
                     windower.add_to_chat(207, _addon.name .. ": new version found (%s -> %s), updating.":format(_addon.version, version))
 
                     -- Open the file in write mode
@@ -55,17 +53,17 @@ windower.register_event('load', function()
                         windower.add_to_chat(207, _addon.name .. ": Update successful.")
                         send_command('@wait 0.5;lua reload ' .. _addon.name)
                         return
-                        
+
                     else
                         windower.add_to_chat(207, _addon.name .. "Failed to open file: " .. file_path)
-                        
-                    end                
-                
+
+                    end
+
                 end
-                
+
             else
                 windower.add_to_chat(207, _addon.name .. ": " .. code .. " Failed to get file.")
-                
+
             end
         end)
         
@@ -77,7 +75,7 @@ windower.register_event('incoming text', function(org, mod, mode_org, mode_mod, 
         return
     end
     local changed = false
-    
+
     if settings.timestamps then
         local changed = true
         mod = tostring(mod):gsub('(%d%d+) seconds', function(str)
@@ -85,7 +83,7 @@ windower.register_event('incoming text', function(org, mod, mode_org, mode_mod, 
             return tonumber(str) >= 60 and os.date("%X", str) or str..' seconds'
         end)
     end
-    
+
     if settings.bignumbers then
         local changed = true      
         mod = mod:gsub('([1-9]%d%d%d+)', function(str)
@@ -104,6 +102,6 @@ windower.register_event('addon command', function(cmd,sep)
         config.save(settings)
         return
     end
-    
+
     send_command('lua r '.._addon.name)
 end)
